2249c1144743733eedcd0cd0264ef54ab2f97905
alexjoabraham <alexjoabraham2@gmail.com>
2024-08-09 02:51:22 -0400

    Added Products page

--- Products.aspx
+++ Products.aspx
@@ -1,15 +1,78 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="TechBuy_FinalProject.Products" %>
+﻿<%@ Page Title="Products Page - TechBuy" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="TechBuy_FinalProject.Products" %>
 
-<!DOCTYPE html>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="products-page-background">
+        <div class="container mt-5">
+            <h1 class="display-4 text-center mb-4">Welcome to TechBuy</h1>
+            <asp:Literal ID="WelcomeMessage" runat="server"></asp:Literal>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
+            <h2 class="text-center mb-4">Our Products</h2>
+
+            <asp:Label ID="MessageLabel" runat="server" CssClass="message text-center" />
+
+            <div class="row mb-4">
+                <!-- Dropdowns Column -->
+                <div class="col-md-3 mb-3">
+                    <asp:DropDownList ID="BrandDropDown" runat="server" CssClass="form-select" AutoPostBack="true" OnSelectedIndexChanged="BrandDropDown_SelectedIndexChanged">
+                        <asp:ListItem Text="Select Brand" Value="0" />
+                    </asp:DropDownList>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <asp:DropDownList ID="MobileDropDown" runat="server" CssClass="form-select" AutoPostBack="true" OnSelectedIndexChanged="MobileDropDown_SelectedIndexChanged">
+                        <asp:ListItem Text="Select Mobile" Value="0" />
+                    </asp:DropDownList>
+                </div>
+
+                <!-- Cards Column -->
+                <div class="col-md-6">
+                    <div class="center-card-container">
+                        <asp:Repeater ID="MobilesRepeater" runat="server" DataSourceID="MobilesByBrandSqlDataSource">
+                            <ItemTemplate>
+                                <div class="mobile-card card flex-row">
+                                    <div class="card-img-left">
+                                        <img src='<%# Eval("Image") %>' alt='<%# Eval("Title") %>' />
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title"><%# Eval("Title") %></h5>
+                                        <p class="card-text"><strong>Brand:</strong> <%# Eval("BrandName") %></p>
+                                        <p class="card-text"><strong>Description:</strong> <%# Eval("Description") %></p>
+                                        <p class="card-text"><strong>Price:</strong> <%# Eval("Price", "${0}") %></p>
+                                        <div class="form-group mb-3">
+                                            <asp:Label ID="QuantityLabel" runat="server" Text="Quantity:" CssClass="form-label" />
+                                            <asp:DropDownList ID="QuantityDropDown" runat="server" CssClass="form-select">
+                                                <asp:ListItem Text="0" Value="0" />
+                                                <asp:ListItem Text="1" Value="1" />
+                                                <asp:ListItem Text="2" Value="2" />
+                                                <asp:ListItem Text="3" Value="3" />
+                                                <asp:ListItem Text="4" Value="4" />
+                                                <asp:ListItem Text="5" Value="5" />
+                                            </asp:DropDownList>
+                                            <asp:CustomValidator ID="QuantityCustomValidator" runat="server"
+                                                ControlToValidate="QuantityDropDown"
+                                                ErrorMessage="Please select a quantity greater than zero."
+                                                CssClass="text-danger"
+                                                OnServerValidate="ValidateQuantity"
+                                                Display="Dynamic" />
+                                        </div>
+                                        <asp:Button ID="AddToCartButton" runat="server" Text="Add to Cart" CommandArgument='<%# Eval("MobileId") %>' CssClass="btn btn-dark w-100" OnClick="AddToCartButton_Click" />
+                                    </div>
+                                </div>
+                            </ItemTemplate>
+                        </asp:Repeater>
+                    </div>
+                </div>
+            </div>
+
+            <asp:SqlDataSource ID="MobilesByBrandSqlDataSource" runat="server"
+                ConnectionString="<%$ ConnectionStrings:TechBuyConnectionString %>"
+                SelectCommand="SELECT m.MobileId, m.Title, m.Description, b.BrandName, m.Price, m.Image 
+                               FROM Mobiles m
+                               INNER JOIN Brands b ON m.BrandId = b.BrandId
+                               WHERE (@MobileId IS NULL OR m.MobileId = @MobileId)">
+                <SelectParameters>
+                    <asp:Parameter Name="MobileId" Type="String" DefaultValue="" />
+                </SelectParameters>
+            </asp:SqlDataSource>
         </div>
-    </form>
-</body>
-</html>
+    </div>
+</asp:Content>
--- Products.aspx.cs
+++ Products.aspx.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Data.SqlClient;
 using System.Web.UI.WebControls;
+using TechBuy_FinalProject.Model;
 
 namespace TechBuy_FinalProject
 {
@@ -11,7 +10,159 @@ namespace TechBuy_FinalProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string username = Session["Username"] as string;
 
+                if (!string.IsNullOrEmpty(username))
+                {
+                    WelcomeMessage.Text = $"<h4 class='welcome-message'>Welcome, {username}!</h4>";
+                }
+
+                // Load brands on initial page load
+                LoadBrands();
+                MobileDropDown.Enabled = false; // Initially disable the Mobile dropdown
+            }
+        }
+
+        private void LoadBrands()
+        {
+            using (var connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TechBuyConnectionString"].ConnectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand("SELECT BrandId, BrandName FROM Brands", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            BrandDropDown.Items.Add(new ListItem(reader["BrandName"].ToString(), reader["BrandId"].ToString()));
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void BrandDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MobileDropDown.Items.Clear();
+            MobileDropDown.Items.Add(new ListItem("Select Mobile", "0"));
+
+            string brandId = BrandDropDown.SelectedValue;
+            if (brandId != "0")
+            {
+                LoadMobilesByBrand(brandId);
+                MobileDropDown.Enabled = true;
+            }
+            else
+            {
+                MobileDropDown.Enabled = false;
+            }
+
+            MobilesRepeater.DataSource = null;
+            MobilesRepeater.DataBind();
+        }
+
+        private void LoadMobilesByBrand(string brandId)
+        {
+            using (var connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TechBuyConnectionString"].ConnectionString))
+            {
+                connection.Open();
+                string query = @"
+                    SELECT MobileId, Title, Description, Price, Image 
+                    FROM Mobiles 
+                    WHERE BrandId = @BrandId
+                ";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@BrandId", brandId);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MobileDropDown.Items.Add(new ListItem(reader["Title"].ToString(), reader["MobileId"].ToString()));
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void MobileDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string mobileId = MobileDropDown.SelectedValue;
+            if (mobileId != "0")
+            {
+                MobilesByBrandSqlDataSource.SelectParameters["MobileId"].DefaultValue = mobileId;
+                MobilesRepeater.DataBind();
+            }
+            else
+            {
+                MobilesRepeater.DataSource = null;
+                MobilesRepeater.DataBind();
+            }
+        }
+
+        protected void AddToCartButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var repeaterItem = (RepeaterItem)button.NamingContainer;
+            var quantityDropDown = (DropDownList)repeaterItem.FindControl("QuantityDropDown");
+            var quantityValidator = (CustomValidator)repeaterItem.FindControl("QuantityCustomValidator");
+            var mobileId = button.CommandArgument;
+
+            quantityValidator.Validate();
+
+            if (quantityValidator.IsValid)
+            {
+                int quantity = int.Parse(quantityDropDown.SelectedValue);
+
+                using (var connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TechBuyConnectionString"].ConnectionString))
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand("SELECT * FROM Mobiles WHERE MobileId = @MobileId", connection))
+                    {
+                        command.Parameters.AddWithValue("@MobileId", mobileId);
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                var mobile = new Mobile
+                                {
+                                    MobileId = reader["MobileId"].ToString(),
+                                    Title = reader["Title"].ToString(),
+                                    Description = reader["Description"].ToString(),
+                                    BrandId = reader["BrandId"].ToString(),
+                                    Image = reader["Image"].ToString(),
+                                    Price = Convert.ToDecimal(reader["Price"])
+                                };
+                                var cart = Session["Cart"] as List<Mobile> ?? new List<Mobile>();
+
+                                for (int i = 0; i < quantity; i++)
+                                {
+                                    cart.Add(mobile);
+                                }
+
+                                Session["Cart"] = cart;
+
+                                MessageLabel.Text = "Item(s) added to cart successfully!";
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageLabel.Text = "Please select a valid quantity.";
+            }
+        }
+
+        protected void ValidateQuantity(object source, ServerValidateEventArgs args)
+        {
+            var validator = (CustomValidator)source;
+            var repeaterItem = (RepeaterItem)validator.NamingContainer;
+            var quantityDropDown = (DropDownList)repeaterItem.FindControl("QuantityDropDown");
+            args.IsValid = quantityDropDown.SelectedValue != "0";
         }
     }
 }
